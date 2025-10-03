5f8c8873e7bce4eb6b7e136e9fa2bbc5279ee551
Родион <rlolokmirgngpn@gmail.com>
2025-10-03 12:52:15 +0300

    Add files via upload

---
+++ ipo-lr-2/task-4/main.py
@@ -0,0 +1,6 @@
+import math
+x = float(input('Введите значение x:' ' '))
+y = float(input('Введите значение y:' ' '))
+z = float(input('Введите значение z:' ' '))
+w = pow((abs(cos(x) - cas(y))) , 1 + 2 * pow(sin(y) , 2)) * (1 + z + (pow(z) , 2) / 2) + ((pow(z) , 3) / 2) + ((pow(x) , 4) / 2) 
+print(w)
